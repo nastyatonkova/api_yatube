8ccb885940b2fc211753efbcc0bedbaf1970e8a9
Anastasiia Tonkova <nast-ton@mail.ru>
2022-06-24 17:17:07 +0200

    deleted urls and views from posts

--- yatube_api/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from posts.views import PostViewSet
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter() 
-router.register('api/v1/posts', PostViewSet) 
-urlpatterns = [
-    path('', include(router.urls)),]
